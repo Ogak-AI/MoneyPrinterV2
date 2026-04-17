9c87afbd4ed84c5305690cfcb63b0f3768e2129e
Ogak-AI <finomoogak2002@gmail.com>
2026-04-17 21:36:30 +0100

    #

--- src/constants.py
+++ src/constants.py
@@ -5,9 +5,6 @@ This file contains all the constants used in the program.
 import os
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-TWITTER_TEXTAREA_CLASS = "public-DraftStyleDefault-block public-DraftStyleDefault-ltr"
-TWITTER_POST_BUTTON_XPATH = "/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/div[3]"
-
 OPTIONS = [
     "YouTube Shorts Automation",
     "Twitter Bot",
@@ -44,13 +41,7 @@ YOUTUBE_CRON_OPTIONS = [
     "Quit"
 ]
 
-# YouTube Section
-YOUTUBE_TEXTBOX_ID = "textbox"
-YOUTUBE_MADE_FOR_KIDS_NAME = "VIDEO_MADE_FOR_KIDS_MFK"
-YOUTUBE_NOT_MADE_FOR_KIDS_NAME = "VIDEO_MADE_FOR_KIDS_NOT_MFK"
-YOUTUBE_NEXT_BUTTON_ID = "next-button"
-YOUTUBE_RADIO_BUTTON_XPATH = "//*[@id=\"radioLabel\"]"
-YOUTUBE_DONE_BUTTON_ID = "done-button"
+
 
 # Amazon Section (AFM)$
 AMAZON_PRODUCT_TITLE_ID = "productTitle"
